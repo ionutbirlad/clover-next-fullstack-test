424c4c1273a6583bd70ee77622cca43da302624c
ionutbirlad <birladionut@yahoo.it>
2026-06-26 11:37:59 +0200

    refactor: use a `single source of truth` for theme variables so that can be shared easily across Ant Tailwind and css files

--- FrontEnd/src/helpers/core/ThemeProvider.jsx
+++ FrontEnd/src/helpers/core/ThemeProvider.jsx
@@ -3,17 +3,37 @@ import { ConfigProvider, theme, App } from 'antd';
 
 import AppContext from '../AppContext';
 import { light, dark } from '../../theme/ant.config';
+import tokens from '../../styles/core/tokens/tokens';
 
 const { defaultAlgorithm, darkAlgorithm } = theme;
 
+const cssColorVariables = {
+  '--color-primary': 'colorPrimary',
+  '--color-success': 'colorSuccess',
+  '--color-warning': 'colorWarning',
+  '--color-error': 'colorError',
+  '--color-info': 'colorInfo',
+  '--color-secondary': 'colorTextSecondary'
+};
+
+const applyCssColorVariables = themeTokens => {
+  Object.entries(cssColorVariables).forEach(([cssVariable, tokenName]) => {
+    document.body.style.setProperty(cssVariable, themeTokens[tokenName]);
+  });
+};
+
 const ThemeProvider = ({ children }) => {
   const { darkMode } = useContext(AppContext);
+  const selectedTheme = darkMode ? dark : light;
+  const selectedTokens = darkMode ? tokens.dark : tokens.light;
 
   if (darkMode) document.body.classList.add('dark');
   else document.body.classList.remove('dark');
 
+  applyCssColorVariables(selectedTokens);
+
   return (
-    <ConfigProvider theme={{ algorithm: darkMode ? darkAlgorithm : defaultAlgorithm, ...(darkMode ? dark : light) }}>
+    <ConfigProvider theme={{ algorithm: darkMode ? darkAlgorithm : defaultAlgorithm, ...selectedTheme }}>
       <App>{children}</App>
     </ConfigProvider>
   );
